d0643325db47d5962c2f415b7f64ee5d7742317a
Ritesh Pramanik <pramanikritesh907@gmail.com>
2025-06-25 21:22:50 +0530

    added route for access token

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import TelegramUserViewSet, RegisterViewSet, LoginViewSet
+# For token refresh functionality
+from rest_framework_simplejwt.views import TokenRefreshView
 
 # Define URL patterns for the API
 # This file contains the URL routing for the API endpoints related to Telegram users, registration, and login.
@@ -9,4 +11,5 @@ urlpatterns = [
     path('users/<int:pk>/', TelegramUserViewSet.as_view({'get': 'retrieve', 'put' : 'update'}), name='telegram-user-detail'),
     path('register/', RegisterViewSet.as_view(), name='register'),
     path('login/', LoginViewSet.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # For getting new access token
 ]
