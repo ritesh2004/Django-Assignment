25197a51a7d82cd020803b0a56749d95eec81c63
Ritesh Pramanik <pramanikritesh907@gmail.com>
2025-06-25 21:01:57 +0530

    Build serializers for auth and telegram user methods

---
+++ api/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import TelegramUsers
+from django.contrib.auth.models import User
+
+# Serializer for User model
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+        
+# Serializer for user registration
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+ 
+# Serializer for user login 
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+        
+# Serializer for Telegram users
+class TelegramUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TelegramUsers
+        fields = ['id', 'username', 'createdAt']
+        read_only_fields = ['createdAt']
+    
+    def create(self, validated_data):
+        return TelegramUsers.objects.create(**validated_data)
