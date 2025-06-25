f68fed2692985237561442d3f2852fe3cd6e7346
Ritesh Pramanik <pramanikritesh907@gmail.com>
2025-06-25 21:01:05 +0530

    Telegram User model created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class TelegramUsers(models.Model):
+    username = models.CharField(max_length=255, blank=True, null=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"TelegramUser(id={self.id}, username={self.username})"
